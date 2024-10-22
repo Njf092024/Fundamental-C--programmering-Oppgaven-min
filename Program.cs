fdf320fb3a21e9e9d39c261a710d7d200c9c6501
Njf092024 <Njf092024@gmail.com>
2024-10-22 11:00:50 +0200

    Steg 1: fork og innfør

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿﻿namespace CS_Tests;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a new instance of the Greeting class
+        Greetings greetings = new Greetings();
+        // English greeting
+        greetings.SayHello();
+        // Japanese greeting
+        greetings.YoiTsuitachi();
+
+    }
+}
